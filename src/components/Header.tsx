eb32db40fdab24e0beb1e5259e1564508b1157b9
Kartik Nain <kartik.nain.9@gmail.com>
2023-06-12 15:43:52 -0400

    Added categories selection feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const header = () => {
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
             </a>
             <div className="flex items-center lg:order-2">
-                <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
+                <NavLink to={'/login'} className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</NavLink>
                 <a href="#" className="text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
                 <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
--- src/pages/AllUsersBlogs.tsx
+++ src/pages/AllUsersBlogs.tsx
@@ -22,6 +22,12 @@ const AllUsersBlogs = () => {
     setActiveTab(tab);
   };
 
+  const [activeCategory, setActiveCategory] = useState<String>("");
+
+  function handleCategoryChange(category: string) {
+    setActiveCategory((prevCategory) => prevCategory === category ? "" : category);
+  }
+
   const AuthContext = useAuth();
 
   return (
@@ -33,8 +39,10 @@ const AllUsersBlogs = () => {
           <div className="w-full md:w-2/3 px-4 mb-8 md:mb-0 order-last md:order-first">
             <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-4">
               {/* Replace with your main article content */}
-              <h2 className="text-2xl font-bold mb-4 dark:text-white">Main Article</h2>
-  
+              <h2 className="text-2xl font-bold mb-4 dark:text-white">
+                Main Article
+              </h2>
+
               {/* Add more main article content as needed */}
               {/* Tabs */}
               <div className="flex space-x-4 mb-4">
@@ -59,13 +67,15 @@ const AllUsersBlogs = () => {
                   By You
                 </button>
               </div>
-  
+
               {/* Content */}
               {activeTab === "forYou" && (
                 <div>
                   {/* Render content for "For You" tab */}
-                  {blogsList.map((b) => (
-                    <Blog {...b} key={b.id} />
+                  {blogsList
+                    .filter((b) => b.category===activeCategory  || activeCategory === "")
+                    .map((b) => (
+                      <Blog {...b} key={b.id} />
                   ))}
                   {/* Add your content here */}
                 </div>
@@ -75,6 +85,7 @@ const AllUsersBlogs = () => {
                   {AuthContext.getUserId()}
                   {blogsList
                     .filter((b) => b.userId === AuthContext.userId)
+                    .filter((b) => b.category===activeCategory  || activeCategory === "")
                     .map((b) => (
                       <Blog {...b} key={b.id} />
                     ))}
@@ -83,21 +94,37 @@ const AllUsersBlogs = () => {
               )}
             </div>
           </div>
-  
+
           {/* Sidebar */}
           <div className="w-full md:w-1/3 px-4 order-first md:order-last">
             <div className="sticky top-16">
               <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-4">
                 {/* Replace with your sidebar content */}
-                <h2 className="text-2xl font-bold mb-4 dark:text-white">Sidebar</h2>
-                <p className="text-white">Some sidebar content goes here.</p>
+                <h2 className="text-2xl font-bold mb-4 dark:text-white">
+                  Categories
+                </h2>
+                {[...new Set(blogsList.map((b) => b.category.toLowerCase()))].map(
+                  (category) => (
+                    <button
+                      className={`px-4 py-2 rounded-lg ${
+                        activeCategory === category
+                          ? "text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 mr-2"
+                          : "bg-gray-200 text-gray-800 dark:bg-gray-800 dark:text-gray-200 mr-2"
+                      }`}
+                      onClick={() => handleCategoryChange(category)}
+                      key={category} // Add a unique key for each button
+                    >
+                      {category}
+                    </button>
+                  )
+                )}
                 {/* Add more sidebar content as needed */}
               </div>
             </div>
           </div>
         </div>
       </div>
-  
+
       {/* Footer */}
       <footer className="bg-gray-900 text-white py-4">
         <div className="container mx-auto px-4">
