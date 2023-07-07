1c469cd39f323df45345ffbd7934c8a2ab535527
Kartik Nain <kartik.nain.9@gmail.com>
2023-07-06 22:07:32 -0400

    Updated backend's baseurl

--- src/api/ApiClient.tsx
+++ src/api/ApiClient.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const ApiClient = axios.create({
-  // baseURL: "https://apricot-squid-hem.cyclic.app/",
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://apricot-squid-hem.cyclic.app/",
+  // baseURL: "http://localhost:3000/",
 });
 
 export default ApiClient;
