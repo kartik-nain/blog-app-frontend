e4daa1bac11c8817107a163c444d049cdd32e1a6
Bharat Raj Verma <bharat.raj1508@gmail.com>
2023-07-13 12:15:49 -0400

    changes to tconfig to rectify deployment issue

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -122,7 +122,6 @@ const Login = () => {
               </div>
               <button
                 type="submit"
-                style={{ color: "red" }}
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700">
                 Sign In
               </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,58 @@
 export const content = [
-  './src/**/*.{js,jsx,ts,tsx}',
-  'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  "./src/**/*.{js,jsx,ts,tsx}",
+  "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
 ];
 export const theme = {
   extend: {
     colors: {
       primary: {
-        '50': '#fdf4ff',
-        '100': '#fae8ff',
-        '200': '#f5d0fe',
-        '300': '#f0abfc',
-        '400': '#e879f9',
-        // '500': '#d946ef',
-        '600': '#19d0c5',
-        '700': '#22e2af',
-        '800': '#86198f',
-        '900': '#701a75'
-      }
+        50: "#fdf4ff",
+        100: "#fae8ff",
+        200: "#f5d0fe",
+        300: "#f0abfc",
+        400: "#e879f9",
+        500: "#d946ef",
+        600: "#19d0c5",
+        700: "#22e2af",
+        800: "#86198f",
+        900: "#701a75",
+      },
     },
     fontFamily: {
-      'body': [
-        'Nunito Sans',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'Segoe UI',
-        'Roboto',
-        'Helvetica Neue',
-        'Arial',
-        'Noto Sans',
-        'sans-serif',
-        'Apple Color Emoji',
-        'Segoe UI Emoji',
-        'Segoe UI Symbol',
-        'Noto Color Emoji'
+      body: [
+        "Nunito Sans",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
       ],
-      'sans': [
-        'Nunito Sans',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'Segoe UI',
-        'Roboto',
-        'Helvetica Neue',
-        'Arial',
-        'Noto Sans',
-        'sans-serif',
-        'Apple Color Emoji',
-        'Segoe UI Emoji',
-        'Segoe UI Symbol',
-        'Noto Color Emoji'
-      ]
-    }
+      sans: [
+        "Nunito Sans",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
   },
 };
-export const plugins = [
-  require('flowbite/plugin')
-];
-export const darkMode = 'media';
+export const plugins = [require("flowbite/plugin")];
+export const darkMode = "media";
